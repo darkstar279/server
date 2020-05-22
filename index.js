e8f76e1e7d6a3cca959fc040ee6197c4ac04e48c
darkstar279 <63704417+darkstar279@users.noreply.github.com>
2020-05-22 22:10:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("menu");
+var nav = document.getElementById("nav");
+var exit = document.getElementById("exit");
+menu.addEventListener("click", function(e){
+    nav.classList.toggle("hide-mobile");
+    e.preventDefault();
+});
+exit.addEventListener("click", function(f){
+    nav.classList.add("hide-mobile");
+    f.preventDefault();
+};
